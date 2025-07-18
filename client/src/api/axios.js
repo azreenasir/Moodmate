b511b495fe022f016430f87946c1fae4628f62ae
azree <azreenasir99@gmail.com>
2025-07-19 01:32:12 +0800

    missing variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // Your backend
+  baseURL: process.env.REACT_APP_API_URL, //backend uRL
+  withCredentials: true,
 });
 
 export default instance;
